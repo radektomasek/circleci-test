1a0bf51ac00fd40150322a09b662f394e2627c87
Radek Tomasek <radek.tomasek@gmail.com>
2019-06-05 13:41:03 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { render } from "react-dom";
+
+const TestComponent = () => <div>Test Component</div>;
+
+render(<TestComponent />, document.getElementById("root"));
