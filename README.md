bbfb8e7ed599196ec99dda347e8140d3f95b5dec
Radek Tomasek <radek.tomasek@gmail.com>
2019-06-06 14:55:30 +0100

    feat(component): text update
    Added some extra test to see a version change.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # circleci-test
-This repository is just for testing CircleCI scripts
+
+This repository is just for testing CircleCI scripts.
+
+A simple flow checks whether a new version is released and prints out the actual number.
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
 
-const TestComponent = () => <div>Test Component</div>;
+const TestComponent = () => <div>Test Component New Text</div>;
 
 render(<TestComponent />, document.getElementById("root"));
